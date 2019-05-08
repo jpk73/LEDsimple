ee29e6a51cb4db61a14850f3f0ae2341996b2f8f
jpk73 <32308624+jpk73@users.noreply.github.com>
2019-05-08 13:58:33 +0200

    Create simple.ino

---
+++ examples/simple/simple.ino
@@ -0,0 +1,45 @@
+
+/*
+
+LEDsimple.h - simple library for LEDs
+inspired by LEDEffect by Harrison H. Jones https://github.com/harrisonhjones/LEDEffect
+
+*/
+
+#include <LEDsimple.h>
+
+LEDsimple led1(55);
+LEDsimple led2(56);
+
+int state;
+int stopwatch;
+
+void setup() {}
+
+void loop()  {
+  if (millis() > stopwatch + 2000) {
+    stopwatch = millis();
+    if (state == 0) {
+      led1.on();
+      led2.off();
+      state = 1;
+    }
+    else if (state == 1) {
+      led1.blink(200);
+      led2.on();
+      state = 2;
+    }
+    else if (state == 2) {
+      led1.off();
+      led2.off();
+      state = 3;
+    }
+    else {
+      led1.blinkOnce(200);
+      led2.on();
+      state = 0;
+    }
+  }
+  led1.update();
+  led2.update();
+}
