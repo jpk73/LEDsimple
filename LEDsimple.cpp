d2ff89b6be4289a13f03466d1bf77d284a24cf6f
jpk73 <32308624+jpk73@users.noreply.github.com>
2019-05-19 14:59:56 +0200

    Update LEDsimple.cpp

--- LEDsimple.cpp
+++ LEDsimple.cpp
@@ -8,21 +8,20 @@ LEDsimple::LEDsimple(int pin) {
   _time = millis();
   _state = 0;
   _state_old = 0;
-  _state_flag = 0;
-  _ledState = 0; // 1 = off, 2 = on, 3 = blink, 4 = blinkOnce
-  _ledDelay = 0; // in ms
+  _counter = 0;
+  _mode = 0;         // 1 = off, 2 = on, 3 = blink, 4 = blink with counter
+  _stopwatch = 1000; // in ms
 }
 
 void LEDsimple::update() {
-  if ((millis() - _time) > _ledDelay) {
+  if ((millis() - _time) > _stopwatch) {
     _time = millis();
-    if (_ledState == 1) {_state = 0;}
-    else if (_ledState == 2) {_state = 1;}
-    else if (_ledState == 3) {_state = !_state;}
-    else if (_ledState == 4) {
-      if (_state == 1 && _state_flag == 1) {_state = 0; _state_flag = 2;}
-      else if (_state == 0 && _state_flag >= 1) {_state = 1; _state_flag = 3;}
-      else if (_state == 1 && _state_flag == 3) {_state = 0; _state_flag = 0; _ledDelay = 1000;}
+    if (_mode == 1) {_state = 0;}
+    else if (_mode == 2) {_state = 1;}
+    else if (_mode == 3) {_state = !_state;}
+    else if (_mode == 4) {
+      if (_counter > 0) {_counter--; _state = !_state;}
+      else {if (_state) {_state = 0;} _stopwatch = 1000;}
     }
     if (_state != _state_old) {
       digitalWrite(_pin, _state);
@@ -32,26 +31,26 @@ void LEDsimple::update() {
 }
 
 void LEDsimple::off() {
-  _ledState = 1;
-  _ledDelay = 1000;
+  _mode = 1;
+  _stopwatch = 1000;
   _time = 0;
 }
 
 void LEDsimple::on() {
-  _ledState = 2;
-  _ledDelay = 1000;
+  _mode = 2;
+  _stopwatch = 1000;
   _time = 0;
 }
 
-void LEDsimple::blink(int ledDelay) {
-  _ledState = 3;
-  _ledDelay = ledDelay;
-  _time = ledDelay;
+void LEDsimple::blink(uint16_t stopwatch) {
+  _mode = 3;
+  _stopwatch = stopwatch;
+  _time = stopwatch;
 }
 
-void LEDsimple::blinkOnce(int ledDelay) {
-  _ledState = 4;
-  _state_flag = 1;
-  _ledDelay = ledDelay;
-  _time = ledDelay;
+void LEDsimple::blink(uint16_t stopwatch, uint16_t counter) {
+  if (counter == 0) {_mode = 3;}
+  else {_mode = 4; _counter = counter * 2;}
+  _stopwatch = stopwatch;
+  _time = stopwatch;
 }
